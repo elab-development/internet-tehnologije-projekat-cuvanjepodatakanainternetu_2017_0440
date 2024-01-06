ec2292a6299c357ec18323a599a21e1b56eb775f
AnDo1802 <andreja.doncic8888@gmail.com>
2024-01-06 16:07:32 +0100

    kreirane 3 migracije za tabelu user

---
+++ api/database/migrations/2024_01_06_144453_update_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('ime');
+            $table->string('prezime');
+            $table->string('uloga');
+            $table->string('one_drive_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->removeColumn('ime');
+            $table->removeColumn('prezime');
+            $table->removeColumn('uloga');
+            $table->removeColumn('one_drive_token');
+        });
+    }
+};
