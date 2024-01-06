c1a365a99c8834858e1853ad6a415799dedf47c1
AnDo1802 <andreja.doncic8888@gmail.com>
2024-01-06 15:43:46 +0100

    dodat model fajl

--- api/app/Models/Fajl.php
+++ api/app/Models/Fajl.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Fajl extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'naziv', 'tip', 'velicina', 'one_drive_path', 'prava_pristupa', 'korisnik_id', 'firma_id'
+    ];
+
+    // Veza sa korisnikom
+    public function korisnik()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Veza sa firmom
+    public function firma()
+    {
+        return $this->belongsTo(Firma::class);
+    }
 }
