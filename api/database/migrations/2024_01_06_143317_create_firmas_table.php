f648033ec755c0753ee38e7156a9dca996b9a8c8
AnDo1802 <andreja.doncic8888@gmail.com>
2024-01-06 16:21:46 +0100

    testiranje migracija

--- api/database/migrations/2024_01_06_143317_create_firmas_table.php
+++ api/database/migrations/2024_01_06_143317_create_firmas_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->timestamps();
         
             $table->foreign('vlasnik_id')->references('id')->on('users');
-            $table->timestamps();
+            
         });
     }
 
