bf9de2aa8ea6c7918544fa7000de03d31f2ad5cb
MartaGojkovic <mv2gojkovic@gmail.com>
2024-01-27 22:50:16 +0100

    dodate rute

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 import Pocetna from './komponente/pocetna/Pocetna';
 import LoginComponent from './komponente/loginRegistracija/LoginComponent';
 import RegistrationComponent from './komponente/loginRegistracija/RegistrationComponent';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
 function App() {
   return (
-    <div  >
-        <Pocetna></Pocetna>
-        <LoginComponent></LoginComponent>
-        <RegistrationComponent></RegistrationComponent>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Pocetna />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/registration" element={<RegistrationComponent />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
