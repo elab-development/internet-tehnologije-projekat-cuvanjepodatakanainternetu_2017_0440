ecb065e6ee6a611018f218cd5df892c02061e22d
MartaGojkovic <mv2gojkovic@gmail.com>
2024-01-27 22:32:57 +0100

    otklonjena greska 419

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Pocetna from './komponente/pocetna/Pocetna';
 import LoginComponent from './komponente/loginRegistracija/LoginComponent';
+import RegistrationComponent from './komponente/loginRegistracija/RegistrationComponent';
 
 function App() {
   return (
     <div  >
         <Pocetna></Pocetna>
         <LoginComponent></LoginComponent>
+        <RegistrationComponent></RegistrationComponent>
     </div>
   );
 }
